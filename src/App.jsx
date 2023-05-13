22923deba2666b795d719aa585cc67dd35c3b4ff
ehsan-c137 <ehsanmousavi1.79@gmali.com>
2023-05-13 22:21:56 +0330

    make all routes available

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,24 @@ import { useMode } from "./Theme";
 import { Routes, Route } from "react-router-dom";
 import { colorModeContext } from "./context/Theme";
 import { ThemeProvider } from "@mui/material/styles";
+import { Box } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
+
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard/index";
-import { Box } from "@mui/material";
 import Team from "./scenes/team";
 import Contacts from "./scenes/contacts/index";
 import Invoices from "./scenes/Invoices/index";
+import Form from "./scenes/form/index";
+import Calendar from "./scenes/calendar/index";
+import FAQ from "./scenes/faq/index";
+import Bar from "./scenes/Bar/index";
+import Pie from "./scenes/Pie/index";
+import Line from "./scenes/line";
+import Geography from "./scenes/geography";
 
+// TODO craete 404 page
 function App() {
    const [theme, colorMode] = useMode();
 
@@ -31,13 +40,13 @@ function App() {
                         <Route path="/team" element={<Team />} />
                         <Route path="/contacts" element={<Contacts />} />
                         <Route path="/invoices" element={<Invoices />} />
-                        {/* <Route path="/form" element={<Form />} />
+                        <Route path="/form" element={<Form />} />
+                        <Route path="/calendar" element={<Calendar />} />
+                        <Route path="/faq" element={<FAQ />} />
                         <Route path="/bar" element={<Bar />} />
                         <Route path="/pie" element={<Pie />} />
                         <Route path="/line" element={<Line />} />
-                        <Route path="/faq" element={<FAQ />} />
-                        <Route path="/gerography" element={<Geography />} />
-                        <Route path="/calendar" element={<Calendar />} />   */}
+                        <Route path="/geography" element={<Geography />} />
                      </Routes>
                   </main>
                </Box>
